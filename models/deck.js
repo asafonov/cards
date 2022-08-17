e613270966f81671b1d32ec807d136eaa760a839
asafonov <me@asafonov.org>
2022-08-17 19:31:28 +0300

    finish card view

--- models/deck.js
+++ models/deck.js
@@ -1,15 +1,15 @@
 class Deck {
   constructor(numCards) {
     this.suits = ['hearts', 'spades', 'diamonds', 'clubs'] 
-    this.views = ['two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king', 'ace']
-    const numViews = (numCards || 36) / this.suits.length
+    this.values = ['two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king', 'ace']
+    const numValues = (numCards || 36) / this.suits.length
     this.deck = []
 
     for (let i = 0; i < this.suits.length; ++i) {
-      for (let j = 0; j < numViews; ++j) {
+      for (let j = 0; j < numValues; ++j) {
         this.deck.push({
           suit: this.suits[i],
-          view: this.views[this.views.length - 1 - j]
+          value: this.values[this.values.length - 1 - j]
         })
       }
     }
