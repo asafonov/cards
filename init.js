2c638a00a45bef0c6f581cf01eb5dbb01987e612
asafonov <me@asafonov.org>
2022-09-23 16:55:31 +0300

    enable updater

--- init.js
+++ init.js
@@ -2,4 +2,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
   window.asafonov.deck = new Deck()
   const board = new GameBoardView()
   const c = new DurakController(window.asafonov.deck)
+  const updaterView = new UpdaterView('https://raw.githubusercontent.com/asafonov/cards/master/VERSION.txt', 'https://github.com/asafonov/cards.apk/releases/download/{VERSION}/app-release.apk')
+  updaterView.showUpdateDialogIfNeeded()
 })
